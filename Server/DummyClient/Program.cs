d545576c2afdc644f0e328d6a66df899484a1777
yonggil kim <gtmddls41@gmail.com>
2023-01-29 05:59:41 +0900

    Listener 생성
    1

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace DummyClient
 {
@@ -16,31 +17,36 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];// 리스트로 나오는 이유는 ip분산을위함(구글같은경우에는 여러개가있음)
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);// 최종주소(식당으로 치면 ipaddr : 식당주소, 포트번호 : 뒷문,정문)
 
-            // 소켓 설정( 휴대폰설정
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            try 
+            while (true)
             {
-                socket.Connect(endPoint);
-                Console.WriteLine($"Connected To{socket.RemoteEndPoint.ToString()}");//remoteEndPoint : 우리가연결한 반대쪽 대상
+                // 소켓 설정( 휴대폰설정
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try 
+                {
+                    //문지기(서버쪽소켓)에게 입장문의
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connected To{socket.RemoteEndPoint.ToString()}");//remoteEndPoint : 우리가연결한 반대쪽 대상
 
-                //보낸다
-                byte[] sendBuff = Encoding.UTF8.GetBytes("hello im client");
-                int sendBytes = socket.Send(sendBuff);
+                    //보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("hello im client");
+                    int sendBytes = socket.Send(sendBuff);
 
-                //받는다
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = socket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Server] {recvData}");
+                    //받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
 
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                Thread.Sleep(100);
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            //문지기(서버쪽소켓)에게 입장문의
 
         }
     }
--- Server/ServerCore/Listener.cs
+++ Server/ServerCore/Listener.cs
@@ -9,11 +9,12 @@ namespace ServerCore
     class Listener
     {
         Socket _listenSocket;
-
-        public void Init(IPEndPoint endPoint)
+        Action<Socket> _onAcceptHandler;
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
         {
+            _onAcceptHandler += onAcceptHandler;
             //소켓 생성(문지기역할)
-            Socket _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             //바인드( 문지기 교육)
             _listenSocket.Bind(endPoint);
@@ -21,11 +22,37 @@ namespace ServerCore
             //Listen(영업시작)
             //backlog : 최대대기수
             _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();// 한번만 만들어주면 계속 재사용가능함
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
         }
 
-        public Socket Accept()
+        void RegisterAccept(SocketAsyncEventArgs args)
         {
-            return _listenSocket.Accept();
+            args.AcceptSocket = null; //  초기화
+
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+            {// 등록하자말자 Accept가 이루어진 경우(바로 클라이언트가 접속요청이 온 경우)
+                OnAcceptCompleted(null, args);
+            }
+
         }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {//유저가 커넥트 요청이 들어오면 여기로 떨어짐
+            if(args.SocketError == SocketError.Success)
+            {
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);//다음 클라이언트를 위해 다시 이벤트 등록
+
+        }
+
     }
 }
