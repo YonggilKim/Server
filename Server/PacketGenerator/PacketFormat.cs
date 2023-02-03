7c6dd9c5d81859f497bf118264b9af1411b22326
yonggil kim <gtmddls41@gmail.com>
2023-02-03 11:24:05 +0900

    패킷 추가 및 테스트
    1

--- Server/PacketGenerator/PacketFormat.cs
+++ Server/PacketGenerator/PacketFormat.cs
@@ -12,7 +12,7 @@ namespace PacketGenerator
 using System.Collections.Generic;
 using System;
 
-class PacketManager
+public class PacketManager
 {{
     #region Singleton
     static PacketManager _instance = new PacketManager();
@@ -70,9 +70,8 @@ class PacketManager
 }}";
         // {0} 패킷 이름
         public static string managerRegisterFormat =
-@"      _makeFunc.Add((ushort)PacketID.{0}, MakePacket<{0}>);
-        _handler.Add((ushort)PacketID.{0}, PacketHandler.{0}Handler);
-";
+@"		_makeFunc.Add((ushort)PacketID.{0}, MakePacket<{0}>);
+		_handler.Add((ushort)PacketID.{0}, PacketHandler.{0}Handler);";
         // {0} 패킷 이름/번호 목록
         // {1} 패킷 목록
         public static string fileFormat =
@@ -109,7 +108,7 @@ public interface IPacket
         // {3} 멤버변수 Write
         public static string packetFormat =
 @"
-class {0} :IPacket
+public class {0} :IPacket
 {{
     {1}
     public ushort Protocol {{ get {{ return (ushort)PacketID.{0}; }}}}
@@ -198,7 +197,7 @@ count += sizeof(ushort);
 for(int i = 0; i < {1}Len; i++)
 {{
     {0} {1} = new {0}();
-    {1}.Read(s, ref count);
+    {1}.Read(segment, ref count);
     {1}s.Add({1});
 }}";
 
--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -6,9 +6,8 @@ using System.Text;
 
 class PacketHandler
 {
-    public static void C_ChatHandler(PacketSession session, IPacket packet)
+    public static void C_LeaveGameHandler(PacketSession session, IPacket packet)
     {
-        C_Chat chatPacket = packet as C_Chat;
         ClientSession clientSession = session as ClientSession;
 
         if (clientSession.Room == null)
@@ -16,13 +15,22 @@ class PacketHandler
 
         GameRoom room = clientSession.Room;
         room.Push(
-            () => room.Broadcast(clientSession, chatPacket.chat)
+            () => room.Leave(clientSession)
         );
     }
 
-    public static void TestHandler(PacketSession session, IPacket packet)
+    public static void C_MoveHandler(PacketSession session, IPacket packet)
     {
+        C_Move movePacket = packet as C_Move;
+        ClientSession clientSession = session as ClientSession;
 
+        if (clientSession.Room == null)
+            return;
+        Console.WriteLine($"{movePacket.posX} ,{movePacket.posY} , {movePacket.posZ} ");
+        GameRoom room = clientSession.Room;
+        room.Push(
+            () => room.Move(clientSession, movePacket)
+        );
     }
 }
 
